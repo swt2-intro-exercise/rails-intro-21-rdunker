f025636020aae1aa1da89c37bac5a116298a7f88
rdunker <bm.rebekka@googlemail.com>
2021-11-23 18:27:01 +0100

    Add infos to author/show.

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -1,27 +1,19 @@
 require 'rails_helper'
 
 describe "Show author page", type: :feature do
+  before do
+    @author = create :author
+  end
   it "should exist at 'show_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-    @author = create :author
     visit author_path(@author)
   end
 
-  it "should have text inputs for an author's first name, last name, and homepage" do
-    visit new_author_path
-    # these are the standard names given to inputs by the Rails form builder
-    expect(page).to have_field('author[first_name]')
-    expect(page).to have_field('author[last_name]')
-    expect(page).to have_field('author[homepage]')
+  it "should show authors name and homepage" do
+    visit author_path(@author)
+    expect(page).to have_text("Alan Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
   end
 
-  it "should save the author from the filled in data" do
-    visit new_author_path
-    page.fill_in 'author[first_name]', with: 'Albert'
-    page.fill_in 'author[last_name]', with: 'Turing'
-    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
-    find('input[type="submit"]').click
-    expect(Author.where(first_name: 'Albert', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing').count).to eq 1
-  end
 end
 
