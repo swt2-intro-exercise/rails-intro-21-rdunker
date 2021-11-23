7604840aa4c55075e9c22ad0ca9a63b834d0079f
rdunker <bm.rebekka@googlemail.com>
2021-11-23 15:21:26 +0100

    Add authors model

---
+++ app/models/author.rb
@@ -0,0 +1,6 @@
+class Author < ApplicationRecord
+  def name
+    first_name + " " + last_name
+  end
+end
+
---
+++ spec/models/author_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  describe 'columns' do
+    it { is_expected.to have_db_column(:first_name).of_type :string}
+    it { is_expected.to have_db_column(:last_name).of_type :string}
+    it { is_expected.to have_db_column(:homepage).of_type :string}
+  end
+
+  describe 'methods' do
+    it 'should build the complete name' do
+      author = Author.create(first_name: 'Alan', last_name: 'Turing', homepage: '"http://wikipedia.org/Alan_Turing"')
+      expect(author.name).to eq 'Alan Turing'
+    end
+  end
+end
